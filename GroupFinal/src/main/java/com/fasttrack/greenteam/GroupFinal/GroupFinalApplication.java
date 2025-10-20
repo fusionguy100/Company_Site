46832dc3a5e948e35bd039c47ddf1ca9c8ccab56
Michael Kim <kim.michaeljay@gmail.com>
2025-10-20 12:16:37 -0600

    Added scaffold for frontend and backend

---
+++ GroupFinal/src/main/java/com/fasttrack/greenteam/GroupFinal/GroupFinalApplication.java
@@ -0,0 +1,13 @@
+package com.fasttrack.greenteam.GroupFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupFinalApplication.class, args);
+	}
+
+}
---
+++ GroupFinal/src/test/java/com/fasttrack/greenteam/GroupFinal/GroupFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasttrack.greenteam.GroupFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
