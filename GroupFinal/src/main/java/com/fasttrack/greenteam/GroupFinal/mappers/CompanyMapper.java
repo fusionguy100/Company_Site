aced78e8ada672dee5fb74839fa153b6330fb029
AKazi05 <akazi05@nyit.edu>
2025-10-21 19:18:48 -0400

    Fixed CompanyMapper

--- GroupFinal/src/main/java/com/fasttrack/greenteam/GroupFinal/mappers/CompanyMapper.java
+++ GroupFinal/src/main/java/com/fasttrack/greenteam/GroupFinal/mappers/CompanyMapper.java
@@ -1,4 +1,16 @@
 package com.fasttrack.greenteam.GroupFinal.mappers;
 
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.fasttrack.greenteam.GroupFinal.dtos.CompanyRequestDto;
+import com.fasttrack.greenteam.GroupFinal.dtos.CompanyResponseDto;
+import com.fasttrack.greenteam.GroupFinal.entities.Company;
+
+@Mapper(componentModel = "spring")
 public interface CompanyMapper {
+    Company dtoToEntity(CompanyRequestDto companyRequestDto);
+    CompanyResponseDto entityToDto(Company company);
+    List<CompanyResponseDto> entitiesToDtos(List<Company> companies);
 }
