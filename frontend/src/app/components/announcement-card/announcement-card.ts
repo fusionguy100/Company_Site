5b32cd73f4275d433b0f2a029fad9ab3c0c0b31c
adamhisel <ryleeking@RyLees-MacBook-Air.local>
2025-10-22 15:20:25 -0500

    Switching to Vs code

---
+++ frontend/src/app/components/announcement-card/announcement-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-card',
+  imports: [],
+  templateUrl: './announcement-card.html',
+  styleUrl: './announcement-card.css'
+})
+export class AnnouncementCard {
+
+}
