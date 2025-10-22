5138a746314d38694cc08a0717c05f00d4a10725
Michael Kim <kim.michaeljay@gmail.com>
2025-10-22 09:26:01 -0600

    Added guard for admin users
    # Conflicts:
    #	frontend/src/app/app.routes.ts

---
+++ frontend/src/app/components/user-registry/user-registry.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registry',
+  imports: [],
+  templateUrl: './user-registry.html',
+  styleUrl: './user-registry.css'
+})
+export class UserRegistry {
+  constructor() {}
+  
+  ngOnInit() {
+    // Initialization logic here
+  }
+
+  ngOnDestroy() {
+    // Cleanup logic here
+  }
+
+}
--- frontend/src/app/guards/admin-guard-guard.spec.ts
+++ frontend/src/app/guards/admin-guard-guard.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { authGuard } from './auth-guard';
+import { adminGuardGuard } from './admin-guard-guard';
 
-describe('authGuard', () => {
+describe('adminGuardGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => adminGuardGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
