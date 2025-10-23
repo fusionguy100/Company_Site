89df5fdcd5005cb42244d9b7f22544de1932b8b2
Jacob Newell <fusionguy200@hotmail.com>
2025-10-22 19:47:54 -0700

    change auth

--- GroupFinal/src/main/java/com/fasttrack/greenteam/GroupFinal/controllers/LoginController.java
+++ GroupFinal/src/main/java/com/fasttrack/greenteam/GroupFinal/controllers/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/auth")
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class LoginController {
 
